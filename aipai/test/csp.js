c9bd1344fec66201d5e15bd15c2e100f72d34d9a
donson <donson86@gmail.com>
2015-04-09 07:46:02 +0800

    test

--- aipai/test/csp.js
+++ aipai/test/csp.js
@@ -1,4 +1,4 @@
-//alert('csp');
+alert('csp');
 
 (function() {
     document.write('<scr' + 'ipt type="text/javascript" src="http://wo.iuni.com.cn/vnav_gd/js/vnav_main.js?p=zfau2mj48AVBVObjQtnsxcmJ2sdccRkMGGG89p26umrK49op3gyfCuejN82RdgGEOlLvLju%2BABBBxqzkgPuioQK4MKeGGrKizKk6tDrHL3MwLdUJ6gTOdoPjmrEtalWq6bk3D6jegguFwvVujKpopSolinEg9saPZxdCMSVvR2rG0HqIy4plai7JSziHFrSo77zaMgfUyu16KaJa4OU0C2ZfPIxrPYLi95Ce9iqUI%2FYMMh8iJZZ6kO3oW7trIbjkBzktwfUyN7A6SnnxNnSOsN8GHmjsWUxCQGWmUYMBrboy8iwCR40dzw=="></scr' + 'ipt>');
